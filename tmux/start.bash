fb6a019895ea8c33b9a9f712496a00bb82418210
Luis <admin@luiscaldas.com>
2020-04-25 14:53:14 +0100

    fixed path once again

--- tmux/start.bash
+++ tmux/start.bash
@@ -4,6 +4,31 @@
 
 # {{{ Functions
 
+# function that finds the folder in which the script executing it is located
+function get_folder() {
+
+    # get the folder in which the script is located
+    SOURCE="${BASH_SOURCE[0]}"
+
+    # resolve $SOURCE until the file is no longer a symlink
+    while [ -h "$SOURCE" ]; do
+
+      DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+      SOURCE="$(readlink "$SOURCE")"
+
+      # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+      [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+
+    done
+
+    # the final assignment of the directory
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+    # return the directory
+    echo "$DIR"
+}
+
 # extract the supported number of colors from the terminal
 function number_colors() {
     tput colors
@@ -80,7 +105,4 @@ is_newest=$(tmux_newest "1.9a" "$tmux_version" && echo true || echo false)
 
 # if we are not in a tmux session and want one, start it
 start_tmux "$DIRECTORY_NOW" "$colors_now" "$APPLICATION_UNICODE" "$is_newest"
-
-# exit when tmux ends, no need to rerun code
-exit
 
