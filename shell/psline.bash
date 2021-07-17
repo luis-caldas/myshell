6e7d048fc651504cef4f066a096a30a1f0ad6caa
Luis <admin@luiscaldas.com>
2021-07-17 16:10:27 +0100

    added time of execution to PS1 line also rewrote the line to accomodate for dynamic functions on PS1 a space is introduced before each command

--- shell/psline.bash
+++ shell/psline.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# {{{ Colours
+
 ######################################################
 # a simple ps1 line for a good looking bash terminal #
 ######################################################
@@ -19,6 +21,9 @@ RAW_COLOURS=(
 	"$(tput sgr0)"     # reset       10
 )
 
+# }}}
+# {{{ Globals
+
 ####################
 # global variables #
 ####################
@@ -36,6 +41,7 @@ GIT_COLOUR=4
 SSH_WARNING_COLOUR=7
 SSH_DOMAIN_COLOUR=2
 SSH_PORT_COLOUR=1
+EXECUTION_TIME_COLOUR=1
 COMMAND_CODE_GOOD_COLOUR=1
 COMMAND_CODE_BAD_COLOUR=0
 BASH_SYMBOL="$"
@@ -50,36 +56,31 @@ fi
 
 ####################
 
+# }}}
+# {{{ Pre Script
+
 # characters to help count the non printing characters
 LIMITERS=( "\001" "\002" )
+LIMITERS_ECHO=( "\x01" "\x02" )
 
 # iterate over the raw colors and add the limiters
 for (( loop_index = 0; loop_index <= ${#RAW_COLOURS[@]} + 1; loop_index++ )); do
 	COLOURS[$loop_index]="${LIMITERS[0]}${RAW_COLOURS[$loop_index]}${LIMITERS[1]}"
+	COLOURS_ECHO[$loop_index]="${LIMITERS_ECHO[0]}${RAW_COLOURS[$loop_index]}${LIMITERS_ECHO[1]}"
 done
 
-USER_HOSTNAME="${COLOURS[7]}${COLOURS[$USER_NAME_COLOUR]}\u${COLOURS[10]}${COLOURS[7]}@${COLOURS[$HOSTNAME_COLOUR]}\h${COLOURS[10]}"
+# check if we are on a ssh connection
+if [ -n "$SSH_CLIENT" ]; then
+	# execute the ssh line function
+	SSH_LINE="$(ssh_line)\n"
+fi
 
-# dir in which the bash is
-DIR_NOW="${COLOURS[7]}${COLOURS[$DIRECTORY_COLOUR]}\w${COLOURS[10]}"
+# id assignment and file path creation
+ROOT_PID="$BASHPID"
+BASH_ID_FILE_PATH="/dev/shm/${USER}.bashtime.${ROOT_PID}"
 
-# bash symbol
-BASH_SYMBOL_BOLD="${COLOURS[7]}$BASH_SYMBOL${COLOURS[10]}"
-
-# create the directory tab
-DIRECTORY_TAB="${COLOURS[7]}[${COLOURS[10]}$DIR_NOW${COLOURS[7]}]${COLOURS[10]}"
-
-# build the power combo
-POWER_COMBO="${COLOURS[7]}[${COLOURS[10]}$USER_HOSTNAME${COLOURS[7]}]${COLOURS[10]}"
-
-# build the jobs tab
-JOBS_INFO="${COLOURS[7]}[${COLOURS[10]}\j${COLOURS[7]}]${COLOURS[10]}"
-
-# build the bash version tab
-BASH_VERSION="${COLOURS[7]}[${COLOURS[10]}\V${COLOURS[7]}]${COLOURS[10]}"
-
-# build the history and command number tab
-HISTORY_COMMAND="${COLOURS[7]}[${COLOURS[10]}!\!${COLOURS[7]}|${COLOURS[10]}#\#${COLOURS[7]}]${COLOURS[10]}"
+# }}}
+# {{{ Dynamic functions (ran at each time the line updates)
 
 # build the function that will print the color base on the successfulness of the previous command
 get_color() {
@@ -141,33 +142,107 @@ ssh_line() {
 
 }
 
+round_seconds (){
 
-# check if we are on a ssh connection
-if [ -n "$SSH_CLIENT" ]; then
-	# execute the ssh line function
-	SSH_LINE="$(ssh_line)\n"
-fi
+	# variable reasignment
+	end_nr="${2}"
+	start_nr="${1}"
 
-# build the date line
-TIME_DATE="${COLOURS[7]}${COLOURS[$TIME_DATE_COLOUR]}$(date +"%Y/%m/%d")${COLOURS[10]}"
+	# rounds a number to 3 decimal places
+	time_difference="$(echo "$end_nr - $start_nr" | bc)"
 
-# build the time line
-TIME_TIME="${COLOURS[7]}${COLOURS[$TIME_LINE_COLOUR]}$(date +"%H:%M:%S")${COLOURS[10]}"
+	# split into integer and float parts
+	time_integer="$(echo "$time_difference" | cut -d. -f1)"
+	time_float="$(echo "$time_difference" | cut -d. -f2 | head -c 8)"
 
-# buid the date brackets
-TIME_DATE_LINE="${COLOURS[7]}[${COLOURS[10]}$TIME_DATE${COLOURS[7]}]${COLOURS[10]}"
+	# return the number formatted
+	printf "%d.%s" "$time_integer" "$time_float"
 
-# build the time brackets
-TIME_LINE="${COLOURS[7]}[${COLOURS[10]}$TIME_TIME${COLOURS[7]}]${COLOURS[10]}"
+}
 
-# build the success tab
-SUCCESSFULNESS="${COLOURS[7]}[${COLOURS[10]}\$(get_color)\$(print_success)${COLOURS[10]}${COLOURS[7]}]${COLOURS[10]}"
+start_time_ps (){
+	# places the epoch time in ns into shared memory
+	date +%s.%N > "$BASH_ID_FILE_PATH"
+}
 
-# build the whole time line
-TIME="$TIME_DATE_LINE $TIME_LINE $SUCCESSFULNESS"
+stop_time_ps (){
+
+	# reads stored epoch time and subtracts from current
+	end_time="$(date +%s.%N)"
+	start_time="$(cat "$BASH_ID_FILE_PATH")"
+	round_seconds "$start_time" "$end_time"
+
+}
+
+# PS1 builder function so newlines can be added as needed
+# and not controlled by command substitutions
+build_ps1_start() {
+
+	# check if we are running for the first time
+	if [ -f "$BASH_ID_FILE_PATH" ]; then
+
+		# stop time line
+		stop_time_line="${COLOURS_ECHO[7]}${COLOURS_ECHO[$EXECUTION_TIME_COLOUR]}$(stop_time_ps "$ROOT_PID")${COLOURS_ECHO[10]}"
+
+		# build the stop time line with the brackets
+		stop_time_line_brackets="${COLOURS_ECHO[7]}[${COLOURS_ECHO[10]}$stop_time_line${COLOURS_ECHO[7]}]${COLOURS_ECHO[10]}"
+
+		# reasign for ease of use
+		time_exec="\n$stop_time_line_brackets\n"
+
+	fi
+
+	# build the date line
+	time_date="${COLOURS_ECHO[7]}${COLOURS_ECHO[$TIME_DATE_COLOUR]}$(date +"%Y/%m/%d")${COLOURS_ECHO[10]}"
+
+	# build the time line
+	time_time="${COLOURS_ECHO[7]}${COLOURS_ECHO[$TIME_LINE_COLOUR]}$(date +"%H:%M:%S")${COLOURS_ECHO[10]}"
+
+	# buid the date brackets
+	time_date_line="${COLOURS_ECHO[7]}[${COLOURS_ECHO[10]}$time_date${COLOURS_ECHO[7]}]${COLOURS_ECHO[10]}"
+
+	# build the time brackets
+	time_clock="${COLOURS_ECHO[7]}[${COLOURS_ECHO[10]}$time_time${COLOURS_ECHO[7]}]${COLOURS_ECHO[10]}"
+
+	# build the success tab
+	successfulness="${COLOURS_ECHO[7]}[${COLOURS_ECHO[10]}$(get_color)$(print_success)${COLOURS_ECHO[10]}${COLOURS_ECHO[7]}]${COLOURS_ECHO[10]}"
+
+	# build the whole time line
+	time_line="$time_date_line $time_clock $successfulness"
+
+	# join the ssh and time exec to the start ps1
+	printf "%b" "$time_exec$time_line"
+
+}
+
+# }}}
+# {{{ General colours for PS1
+
+USER_HOSTNAME="${COLOURS[7]}${COLOURS[$USER_NAME_COLOUR]}\u${COLOURS[10]}${COLOURS[7]}@${COLOURS[$HOSTNAME_COLOUR]}\h${COLOURS[10]}"
+
+# dir in which the bash is
+DIR_NOW="${COLOURS[7]}${COLOURS[$DIRECTORY_COLOUR]}\w${COLOURS[10]}"
+
+# bash symbol
+BASH_SYMBOL_BOLD="${COLOURS[7]}$BASH_SYMBOL${COLOURS[10]}"
+
+# create the directory tab
+DIRECTORY_TAB="${COLOURS[7]}[${COLOURS[10]}$DIR_NOW${COLOURS[7]}]${COLOURS[10]}"
+
+# build the power combo
+POWER_COMBO="${COLOURS[7]}[${COLOURS[10]}$USER_HOSTNAME${COLOURS[7]}]${COLOURS[10]}"
+
+# build the jobs tab
+#JOBS_INFO="${COLOURS[7]}[${COLOURS[10]}\j${COLOURS[7]}]${COLOURS[10]}"
+
+# build the bash version tab
+BASH_VERSION="${COLOURS[7]}[${COLOURS[10]}\V${COLOURS[7]}]${COLOURS[10]}"
+
+# build the history and command number tab
+#HISTORY_COMMAND="${COLOURS[7]}[${COLOURS[10]}!\!${COLOURS[7]}|${COLOURS[10]}#\#${COLOURS[7]}]${COLOURS[10]}"
 
 # build the information line
-INFORMATION_LINE="$POWER_COMBO $DIRECTORY_TAB\$(maybe_git)$BASH_SYMBOL_BOLD"
+INFORMATION_LINE="$POWER_COMBO $DIRECTORY_TAB$(maybe_git)$BASH_SYMBOL_BOLD"
 
 # build the line in which the command will be executed
 COMMAND_LINE="$BASH_SYMBOL_BOLD ${COLOURS[7]}>${COLOURS[10]} "
@@ -175,11 +250,25 @@ COMMAND_LINE="$BASH_SYMBOL_BOLD ${COLOURS[7]}>${COLOURS[10]} "
 # command line for ps2
 COMMAND_LINE_PS2="${COLOURS[7]}  >${COLOURS[10]} "
 
+# }}}
+# {{{ Final Assignments
+
+# add time counter to PS0
+PS0="\$(start_time_ps ""$ROOT_PID"")\n"
+
 # the save the success code
 PROMPT_COMMAND="SUCCESS_CODE=\$?"
 
 # final ps1 assignment
-PS1="\n$SSH_LINE$TIME\n$INFORMATION_LINE\n$COMMAND_LINE"
+PS1="\$(build_ps1_start)\n$SSH_LINE$INFORMATION_LINE\n$COMMAND_LINE"
 
 # assign ps2 as well while we're at it
 PS2="$COMMAND_LINE_PS2"
+
+# cleanup files on shm
+function run_on_exit (){
+	rm "/dev/shm/${USER}.bashtime.${ROOT_PID}"
+}
+trap run_on_exit EXIT
+
+# }}}
