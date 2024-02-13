78f4f915828f508b4fa7cf0e8bb5cd6aae55c2dc
Luis <luis@caldas.ie>
2024-02-13 16:22:58 +0000

    updated aliases

--- shell/basic.bash
+++ shell/basic.bash
@@ -5,16 +5,6 @@
 # My umask
 umask 0077  # Only user has permissions, also no special bits
 
-# Aliases to my preferred configurations
-alias flush='history -c; history -w'
-alias grip='grip --user=$GIT_API_EMAIL --pass=$GIT_API_KEY --quiet'
-alias nano='nano -l -E -T4'
-
-# VIM aliases
-alias vi='vi -p'
-alias vim='vim -p'
-alias nvim='nvim -p'
-
 # Enable keypad mode
 tput smkx
 
@@ -36,23 +26,22 @@ export EDITOR="vim"
 # Nix-shell force to use own PS1 line
 export NIX_SHELL_PRESERVE_PROMPT="1"
 
-# Tmux aliases
-alias na='tmux a'
-alias nat='tmux a -t'
+# Add the gcc colors as well
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# Aliases
+
+alias cp='cp -i'
+alias mv='mv -i'
 
 # Add colour aliases
 alias ls='ls --group-directories-first --color=auto'
 alias grep='grep --color=auto'
 
-# Some NixOS aliases
-alias reb='nixos-rebuild boot --option tarball-ttl 0'
-alias rebu='nixos-rebuild boot --upgrade --option tarball-ttl 0'
-alias sreb='sudo nixos-rebuild boot --option tarball-ttl 0'
-alias srebu='sudo nixos-rebuild boot --upgrade --option tarball-ttl 0'
-alias resw='nixos-rebuild switch --option tarball-ttl 0'
-alias reswu='nixos-rebuild switch --upgrade --option tarball-ttl 0'
-alias sresw='sudo nixos-rebuild switch --option tarball-ttl 0'
-alias sreswu='sudo nixos-rebuild switch --upgrade --option tarball-ttl 0'
+# VIM aliases
+alias vi='vi -p'
+alias vim='vim -p'
+alias nvim='nvim -p'
 
 # Clock
 alias clock='tty-clock -c -b -s'
@@ -63,8 +52,26 @@ alias phttpd='python -m SimpleHTTPServer'
 # Alias for unmount
 alias unmount='umount'
 
-# Add the gcc colors as well
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+# Aliases to my preferred configurations
+alias flush='history -c; history -w'
+alias grip='grip --user=$GIT_API_EMAIL --pass=$GIT_API_KEY --quiet'
+alias nano='nano -l -E -T4'
+
+# Shortening commands
+
+# Tmux aliases
+alias na='tmux a'
+alias nat='tmux a -t'
+
+# Some NixOS aliases
+alias reb='nixos-rebuild boot --option tarball-ttl 0'
+alias rebu='nixos-rebuild boot --upgrade --option tarball-ttl 0'
+alias sreb='sudo nixos-rebuild boot --option tarball-ttl 0'
+alias srebu='sudo nixos-rebuild boot --upgrade --option tarball-ttl 0'
+alias resw='nixos-rebuild switch --option tarball-ttl 0'
+alias reswu='nixos-rebuild switch --upgrade --option tarball-ttl 0'
+alias sresw='sudo nixos-rebuild switch --option tarball-ttl 0'
+alias sreswu='sudo nixos-rebuild switch --upgrade --option tarball-ttl 0'
 
 # Check if the git autocompletion file exists
 if [ -f "/etc/bash_completion.d/git-prompt" ]; then
