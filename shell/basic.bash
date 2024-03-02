13af02822a35dc530b694cba134be4897136fc6c
Luis <luis@caldas.ie>
2024-03-02 00:27:06 +0000

    added alias to rm to force myself to use trash

--- shell/basic.bash
+++ shell/basic.bash
@@ -33,6 +33,7 @@ export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 
 alias cp='cp -i'
 alias mv='mv -i'
+alist rm='rm -i'
 
 # Add colour aliases
 alias ls='ls --group-directories-first --color=auto'
